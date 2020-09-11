d0585d40a0f240627ae3223b370bccae91e14e10
leowbrown <68573501+leowbrown@users.noreply.github.com>
2020-09-11 21:51:38 +1200

    adjusted homepage jquery

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
 $(document).ready(function(){
 
   $('#login--btn').click(function() {
-  $('.cta--container').toggle(200, 'swing');
-  $('.brand--logo').toggle(200, 'swing');
-  $('#login--pannel').toggleClass("active", 300, 'swing');
-  
+    $('.cta--container').toggle(200, 'swing');
+    $('.brand--logo').toggle(200, 'swing');
+    $('#login--pannel').toggleClass("active", 300, 'swing');
   });
 
-  $('.close--icon').click(function() {
-  $('.cta--container').toggle(200, 'swing');
-  $('.brand--logo').toggle(200, 'swing');
-  $('#login--pannel').toggleClass("active", 300, 'swing');
+  $('.signin--button').click(function() {
+    $('#login--pannel').toggleClass("active", 300, 'swing');
+    $('.cta--container').toggle(200, 'swing');
+    $('.brand--logo').toggle(200, 'swing');
+    $('.cta--header').html("Welcome Back");
+    $('.cta--subheader').html("Its good to see you again.")
+    $('.cta--button').html("Control room")
+  });
 
+  $('#close--btn, .login--logo').click(function() {
+    $('.cta--container').toggle(200, 'swing');
+    $('.brand--logo').toggle(200, 'swing');
+    $('#login--pannel').toggleClass("active", 300, 'swing');
   });
 
 });
